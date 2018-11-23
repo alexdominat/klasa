a3ccb2c23022d3a97eb6c7d6cd4c6c4333c5c5e0
kyranet <kyradiscord@gmail.com>
2018-11-23 11:04:37 +0100

    typings: Update for latest commit

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -642,9 +642,11 @@ declare module 'klasa' {
 	}
 
 	export class TextPrompt {
-		public constructor(message: KlasaMessage, usage: Usage, options: TextPromptOptions);
+		public constructor(message: KlasaMessage, usage: Usage, options?: TextPromptOptions);
 		public readonly client: KlasaClient;
 		public message: KlasaMessage;
+		public target: KlasaUser;
+		public channel: TextChannel | GroupDMChannel | DMChannel;
 		public usage: Usage | CommandUsage;
 		public reprompted: boolean;
 		public flags: ObjectLiteral<string>;
@@ -660,6 +662,7 @@ declare module 'klasa' {
 		private _currentUsage: Tag;
 
 		public run<T = any[]>(prompt: string): Promise<T>;
+		private prompt(text: string): Promise<KlasaMessage>;
 		private reprompt(prompt: string): Promise<any[]>;
 		private repeatingPrompt(): Promise<any[]>;
 		private validateArgs(): Promise<any[]>;
@@ -1241,7 +1244,6 @@ declare module 'klasa' {
 			readonly reprompted: boolean;
 			readonly reactable: boolean;
 			send(content?: StringResolvable, options?: MessageOptions): Promise<KlasaMessage | KlasaMessage[]>;
-			prompt(text: string, time?: number): Promise<KlasaMessage>;
 			usableCommands(): Promise<Collection<string, Command>>;
 			hasAtLeastPermissionLevel(min: number): Promise<boolean>;
 		}
@@ -1678,9 +1680,11 @@ declare module 'klasa' {
 
 	// Usage
 	export type TextPromptOptions = {
+		channel?: TextChannel | GroupDMChannel | DMChannel;
 		limit?: number;
-		time?: number;
 		quotedStringSupport?: boolean;
+		target?: KlasaUser;
+		time?: number;
 	};
 
 	// Util
